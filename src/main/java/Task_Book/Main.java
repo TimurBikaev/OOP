f43115be52eafe034cb748276ad13b71170a2c9e
Timur Bikaev <tbikaev@real-trac.com>
2024-11-28 17:01:07 +0100

    Добавление задач по ООП

--- src/main/java/Task_Book/Main.java
+++ src/main/java/Task_Book/Main.java
@@ -2,7 +2,7 @@ package Task_Book;
 
 public class Main {
     public static void main(String[] args) {
-        Book book = new Book("Мастер и Маргарита", "М. Булгаков", 500, true);
+        Book book = new Book("Мастер и Маргарита", "М. Булгаков", 1910, true);
         book.getBookInfo();
 
         book.borrowBook();
@@ -11,7 +11,7 @@ public class Main {
 
         //создание библиотеки книг
         Library library = new Library();
-        library.addBook("Мастер и Маргарита", "М. Булгаков", 500, true);
+        library.addBook("Мастер и Маргарита", "М. Булгаков", 1910, true);
         library.addBook("1984", "Дж. Оруэлл", 1949, true);
         library.displayAvailableBooks();
 
